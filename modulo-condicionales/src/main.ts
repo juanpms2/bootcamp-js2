8f26745caab6bf64e158a87ad8faebf3e084344f
juanpms2 <juanpablo@lemoncode.net>
2023-04-26 15:40:24 +0200

    shaw card and result

--- modulo-condicionales/src/main.ts
+++ modulo-condicionales/src/main.ts
@@ -1,23 +1,63 @@
 import "./style.css";
 
-let playerScoreOne: number | string = 0;
-const playerOne = document.querySelector('#player-one')!
-const showScoreButton = document.querySelector('#show-score')!
+enum figureCardEnum {
+  AS = 1,
+  DOS = 2,
+  TRES = 3,
+  CUATRO = 4,
+  CINCO = 5,
+  SEIS = 6,
+  SIETE = 7,
+  SOTA = 8,
+  CABALLO = 9,
+  REY = 10,
+};
+
+enum paloEnum {
+  OROS = 1,
+  COPAS = 2,
+  ESPADAS = 3,
+  BASTOS = 4,
+};
+
+let playerScoreOne: number = 0;
+const playerOne = document.querySelector('#player-score-one')!
+const orderCardButton = document.querySelector('#order-card')!
+const orderedCards = document.querySelector('#ordered-cards')!
 
 
-const showScore = (puntuacion: number) => {
+const getRandomInt = (min: number, max: number) => {
+  const minimo = Math.ceil(min);
+  const maximo = Math.floor(max);
+  return Math.floor(Math.random() * (maximo - minimo)) + minimo;
+};
+
+
+const getRandomCard = () => {
+  const palo: paloEnum = getRandomInt(1, 4);
+  const card: figureCardEnum = getRandomInt(1, 10);
+  playerScoreOne = playerScoreOne + card;
+  showScore(playerScoreOne, palo, card);
+  return card;
+};
+
+
+const showScore = (puntuacion: number, palo: paloEnum, figureCard: figureCardEnum) => {
   if (puntuacion > 7.5) {
-    playerScoreOne = 'Has perdido';
+    playerOne.innerHTML = `${puntuacion} Has perdido`;
+    orderedCards.innerHTML = `<img class="card" src="/assets/${paloEnum[palo].toLowerCase()}/${figureCard}.svg" alt="ordered card" />`;
   } else {
-    playerScoreOne = puntuacion;
+    playerOne.innerHTML = puntuacion.toString();
+    orderedCards.innerHTML = `<img class="card" src="/assets/${paloEnum[palo].toLowerCase()}/${figureCard}.svg" alt="ordered card" />`;
   }
   return playerScoreOne;
 };
 
-showScoreButton.addEventListener('click', () => showScore(8));
+const orderCard = () => {
+  const card = getRandomCard();
+  return card;
+};
+
+orderCardButton.addEventListener('click', () => orderCard());
 
 
-
-playerOne.innerHTML = `
-
-`;
