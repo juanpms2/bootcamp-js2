6a5148b9c19fe27dc0555679fe596bb47c082d26
juanpms2 <juanpablo@lemoncode.net>
2023-06-01 14:26:55 +0200

    refactor

--- [8.3]-memory-app/03-mostrar-grid/src/main.ts
+++ [8.3]-memory-app/03-mostrar-grid/src/main.ts
@@ -1,16 +1,14 @@
+import "./style.css";
+
 const createGrid = () => {
 	const root = document.querySelector("#app");
-	const title = document.createElement("h2");
-	title.innerHTML = "03 Mostrar grid";
-	root?.appendChild(title);
-
 	const container = document.createElement("div");
-	container.setAttribute("id", "grid-container");
+	container?.setAttribute("id", "grid-container");
 	root?.appendChild(container);
 
 	for (let i = 0; i < 12; i++) {
 		const card = document.createElement("div");
-		card.setAttribute("class", "card");
+		card?.setAttribute("class", "card");
 		container?.appendChild(card);
 	}
 };
