e52cace94cf2eb1d1a6e70efc461ab9557391955
Braulio Diez Botella <braulio.diez@gmail.com>
2023-06-21 13:33:18 +0200

    wip

--- [8.3]-memory-game/06-memory-game/src/core/motor.ts
+++ [8.3]-memory-game/06-memory-game/src/core/motor.ts
@@ -6,113 +6,113 @@ export const getBoard = (): Board => board;
 export const setBoard = (newBoard: Board): Board => (board = newBoard);
 
 export const shuffleCards = (cards: Card[]): Card[] => {
-	if (cards) {
-		// Fisher-Yates shuffle
-		for (let i = cards.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			const temp = cards[i];
-			cards[i] = cards[j];
-			cards[j] = temp;
-		}
-		return cards;
-	} else {
-		return [];
-	}
+  if (cards) {
+    // Fisher-Yates shuffle
+    for (let i = cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = cards[i];
+      cards[i] = cards[j];
+      cards[j] = temp;
+    }
+    return cards;
+  } else {
+    return [];
+  }
 };
 
 export const canBeFlipped = (card: Card, statusGame: statusGame): boolean =>
-	card?.isFlipped && statusGame === "DosCartasLevantadas" ? false : true;
+  card?.isFlipped || statusGame === "DosCartasLevantadas" ? false : true;
 
 export const flipCard = (index: number, board: Board): void => {
-	const cardList = board.cardList?.map((card, i) =>
-		i === index ? { ...card, isFlipped: true } : card
-	);
-	setBoard({
-		...board,
-		cardList,
-	});
+  const cardList = board.cardList?.map((card, i) =>
+    i === index ? { ...card, isFlipped: true } : card
+  );
+  setBoard({
+    ...board,
+    cardList,
+  });
 };
 
 export const updateStatusGame = (cardIndex: number, board: Board): void => {
-	switch (board.statusGame) {
-		case "CeroCartasLevantadas":
-			setBoard({
-				...board,
-				statusGame: "UnaCartaLevantada",
-				indexCardFlipA: cardIndex,
-			});
-			break;
-		case "UnaCartaLevantada":
-			setBoard({
-				...board,
-				statusGame: "DosCartasLevantadas",
-				indexCardFlipB: cardIndex,
-			});
-			break;
-		case "DosCartasLevantadas":
-			setBoard({
-				...board,
-				statusGame: "CeroCartasLevantadas",
-				indexCardFlipA: null,
-				indexCardFlipB: null,
-			});
-			break;
-		default:
-			break;
-	}
+  switch (board.statusGame) {
+    case "CeroCartasLevantadas":
+      setBoard({
+        ...board,
+        statusGame: "UnaCartaLevantada",
+        indexCardFlipA: cardIndex,
+      });
+      break;
+    case "UnaCartaLevantada":
+      setBoard({
+        ...board,
+        statusGame: "DosCartasLevantadas",
+        indexCardFlipB: cardIndex,
+      });
+      break;
+    case "DosCartasLevantadas":
+      setBoard({
+        ...board,
+        statusGame: "CeroCartasLevantadas",
+        indexCardFlipA: null,
+        indexCardFlipB: null,
+      });
+      break;
+    default:
+      break;
+  }
 };
 
 export const checkMatch = (indexCardB: number, board: Board): boolean =>
-	board.cardList[board.indexCardFlipA].card?.id ===
-	board.cardList[indexCardB].card?.id
-		? true
-		: false;
+  board.cardList[board.indexCardFlipA].card?.id ===
+  board.cardList[indexCardB].card?.id
+    ? true
+    : false;
 
 export const markSelectedPairCardAsMatched = (
-	indexCardB: number,
-	board: Board
+  indexCardB: number,
+  board: Board
 ): void => {
-	const list = board.cardList.map((card, i) =>
-		i === indexCardB || i === board.indexCardFlipA
-			? { ...card, isFound: true }
-			: card
-	);
-	setBoard({
-		...board,
-		cardList: list,
-	});
+  const list = board.cardList.map((card, i) =>
+    i === indexCardB || i === board.indexCardFlipA
+      ? { ...card, isFound: true }
+      : card
+  );
+  setBoard({
+    ...board,
+    cardList: list,
+  });
 };
 
 export const resetSelectedPairCardsEngine = (
-	indexCardB: number,
-	board: Board
+  indexCardB: number,
+  board: Board
 ): void => {
-	const list = board.cardList.map((card, i) =>
-		i === indexCardB || i === board.indexCardFlipA
-			? { ...card, isFlipped: false }
-			: card
-	);
-	setBoard({ ...board, cardList: list });
+  const list = board.cardList.map((card, i) =>
+    i === indexCardB || i === board.indexCardFlipA
+      ? { ...card, isFlipped: false }
+      : card
+  );
+  setBoard({ ...board, cardList: list });
 };
 
 export const updateMoves = (board: Board): void => {
-	setBoard({ ...board, moves: board.moves + 1 });
+  setBoard({ ...board, moves: board.moves + 1 });
 };
 
 export const isGameFinished = (cardList: Card[]): boolean =>
-	cardList.every((card) => card.isFound);
+  cardList.every((card) => card.isFound);
 
 export const markGameToFinished = (board: Board): void => {
-	setBoard({ ...board, statusGame: "PartidaCompleta" });
+  setBoard({ ...board, statusGame: "PartidaCompleta" });
 };
 
 export const startGame = (): void => {
-	const board = getBoard();
-	setBoard({
-		...board,
-		cardList: shuffleCards(board.cardList),
-		statusGame: "CeroCartasLevantadas",
-	});
+  const board = getBoard();
+  setBoard({
+    ...board,
+    cardList: shuffleCards(board.cardList),
+    statusGame: "CeroCartasLevantadas",
+  });
 };
 
 // export const resetGame = (): void => {
@@ -120,9 +120,9 @@ export const startGame = (): void => {
 // };
 
 export const resetFlippedCards = (board: Board): void => {
-	setBoard({
-		...board,
-		indexCardFlipA: null,
-		indexCardFlipB: null,
-	});
+  setBoard({
+    ...board,
+    indexCardFlipA: null,
+    indexCardFlipB: null,
+  });
 };
--- [8.3]-memory-game/06-memory-game/src/core/ui.ts
+++ [8.3]-memory-game/06-memory-game/src/core/ui.ts
@@ -1,20 +1,20 @@
 import { elementReady } from "./helpers";
 import { CardComponent, TooltipComponent } from "../components";
 import {
-	canBeFlipped,
-	checkMatch,
-	startGame,
-	// resetToContinue,
-	getBoard,
-	// resetGame,
-	flipCard,
-	isGameFinished,
-	resetSelectedPairCardsEngine,
-	markSelectedPairCardAsMatched,
-	markGameToFinished,
-	updateStatusGame,
-	updateMoves,
-	resetFlippedCards,
+  canBeFlipped,
+  checkMatch,
+  startGame,
+  // resetToContinue,
+  getBoard,
+  // resetGame,
+  flipCard,
+  isGameFinished,
+  resetSelectedPairCardsEngine,
+  markSelectedPairCardAsMatched,
+  markGameToFinished,
+  updateStatusGame,
+  updateMoves,
+  resetFlippedCards,
 } from "./motor";
 import { defaultScoreboard, textCardTooltip, textScoreboard } from "./constans";
 import { Board } from "./model";
@@ -26,146 +26,146 @@ const gridContainerElement = elementReady("grid-container");
 const scoreboardElement = elementReady("scoreboard");
 
 export const loadApp = () => {
-	const restoreToCardNotFlipped = (
-		indexCardFlipA: number,
-		indexCardFlipB: number
-	) => {
-		setTimeout(() => {
-			document
-				.querySelector(`[data-index-array="${indexCardFlipA}"]`)
-				?.classList.remove("flip");
-			document
-				.querySelector(`[data-index-array="${indexCardFlipB}"]`)
-				?.classList.remove("flip");
-		}, 1000);
-	};
+  const restoreToCardNotFlipped = (
+    indexCardFlipA: number,
+    indexCardFlipB: number
+  ) => {
+    setTimeout(() => {
+      document
+        .querySelector(`[data-index-array="${indexCardFlipA}"]`)
+        ?.classList.remove("flip");
+      document
+        .querySelector(`[data-index-array="${indexCardFlipB}"]`)
+        ?.classList.remove("flip");
+    }, 1000);
+  };
 
-	const displayCardTooltip = (cardIndex: number): void => {
-		const tooltip = TooltipComponent(textCardTooltip);
-		const card = document.querySelector(`[data-index-array="${cardIndex}"]`);
-		card?.appendChild(tooltip);
-		setTimeout(() => {
-			card?.removeChild(tooltip);
-		}, 2000);
-	};
+  const displayCardTooltip = (cardIndex: number): void => {
+    const tooltip = TooltipComponent(textCardTooltip);
+    const card = document.querySelector(`[data-index-array="${cardIndex}"]`);
+    card?.appendChild(tooltip);
+    setTimeout(() => {
+      card?.removeChild(tooltip);
+    }, 2000);
+  };
 
-	const cardIsFlipped = (cardIndex: number, isFlipped: boolean): void => {
-		if (isFlipped) {
-			displayCardTooltip(cardIndex);
-		}
-	};
+  const cardIsFlipped = (cardIndex: number, isFlipped: boolean): void => {
+    if (isFlipped) {
+      displayCardTooltip(cardIndex);
+    }
+  };
 
-	const displayGameResultTooltip = (moves: number): void => {
-		const txt = `<h1>¡Has ganado!</h1> <p>Has completado el juego en ${moves.toString()} movimientos</p>`;
-		const tooltip = TooltipComponent(txt);
-		tooltip.classList.add("tooltip-win");
-		boardContainerElement?.appendChild(tooltip);
-		setTimeout(() => {
-			boardContainerElement?.removeChild(tooltip);
-		}, 5000);
-	};
+  const displayGameResultTooltip = (moves: number): void => {
+    const txt = `<h1>¡Has ganado!</h1> <p>Has completado el juego en ${moves.toString()} movimientos</p>`;
+    const tooltip = TooltipComponent(txt);
+    tooltip.classList.add("tooltip-win");
+    boardContainerElement?.appendChild(tooltip);
+    setTimeout(() => {
+      boardContainerElement?.removeChild(tooltip);
+    }, 5000);
+  };
 
-	const removeGameResultTooltip = (): void => {
-		const tooltipResult = document.querySelector(".tooltip-win");
-		tooltipResult && boardContainerElement?.removeChild(tooltipResult);
-	};
+  const removeGameResultTooltip = (): void => {
+    const tooltipResult = document.querySelector(".tooltip-win");
+    tooltipResult && boardContainerElement?.removeChild(tooltipResult);
+  };
 
-	const checkGameFinished = (board: Board) => {
-		const isFinished = isGameFinished(board.cardList);
+  const checkGameFinished = (board: Board) => {
+    const isFinished = isGameFinished(board.cardList);
 
-		if (isFinished) {
-			markGameToFinished(board);
-			displayGameResultTooltip(board.moves);
-		}
-	};
+    if (isFinished) {
+      markGameToFinished(board);
+      displayGameResultTooltip(board.moves);
+    }
+  };
 
-	const handleUpdateMoves = (board: Board): void => {
-		if (board.statusGame === "UnaCartaLevantada") {
-			scoreboardElement.innerHTML = board.moves + textScoreboard;
-		}
-	};
+  const handleUpdateMoves = (board: Board): void => {
+    if (board.statusGame === "UnaCartaLevantada") {
+      scoreboardElement.innerHTML = board.moves + textScoreboard;
+    }
+  };
 
-	const handleCheckMatch = (indexCardB: number, board: Board): void => {
-		const isMatch = checkMatch(indexCardB, board);
+  const handleCheckMatch = (indexCardB: number, board: Board): void => {
+    const isMatch = checkMatch(indexCardB, board);
 
-		if (isMatch) {
-			console.log("es match");
-			markSelectedPairCardAsMatched(indexCardB, board);
-			updateMoves(board);
-			checkGameFinished(board);
-		} else {
-			console.log("no es match");
-			resetSelectedPairCardsEngine(indexCardB, board);
-			restoreToCardNotFlipped(board.indexCardFlipA, indexCardB);
-			resetFlippedCards(board);
-		}
-		handleUpdateMoves(board);
-	};
+    if (isMatch) {
+      console.log("es match");
+      markSelectedPairCardAsMatched(indexCardB, getBoard());
+      updateMoves(getBoard());
+      checkGameFinished(getBoard());
+    } else {
+      console.log("no es match");
+      resetSelectedPairCardsEngine(indexCardB, getBoard());
+      restoreToCardNotFlipped(board.indexCardFlipA, indexCardB);
+      resetFlippedCards(getBoard());
+    }
+    handleUpdateMoves(getBoard());
+  };
 
-	const handleSecondFlip = (cardIndex: number, board: Board): void => {
-		console.log("handleSecondFlip", board);
-		if (board.statusGame === "UnaCartaLevantada") {
-			handleCheckMatch(cardIndex, board);
-		}
-	};
+  const handleSecondFlip = (cardIndex: number, board: Board): void => {
+    console.log("handleSecondFlip", board);
+    if (board.statusGame === "DosCartasLevantadas") {
+      handleCheckMatch(cardIndex, board);
+    }
+  };
 
-	const handleFlip = (cardIndex: number, board: Board): void => {
-		const flip = canBeFlipped(board.cardList[cardIndex], board.statusGame);
+  const handleFlip = (cardIndex: number, board: Board): void => {
+    const flip = canBeFlipped(board.cardList[cardIndex], board.statusGame);
 
-		if (flip) {
-			flipCard(cardIndex, board);
-			document
-				.querySelector(`[data-index-array="${cardIndex}"]`)
-				?.classList.add("flip");
+    if (flip) {
+      flipCard(cardIndex, getBoard());
+      document
+        .querySelector(`[data-index-array="${cardIndex}"]`)
+        ?.classList.add("flip");
 
-			updateStatusGame(cardIndex, board);
-			handleSecondFlip(cardIndex, board);
-		} else {
-			cardIsFlipped(cardIndex, board.cardList[cardIndex].isFlipped);
-		}
-	};
+      updateStatusGame(cardIndex, getBoard());
+      handleSecondFlip(cardIndex, getBoard());
+    } else {
+      cardIsFlipped(cardIndex, board.cardList[cardIndex].isFlipped);
+    }
+  };
 
-	const createCardList = (): void => {
-		const board = getBoard();
+  const createCardList = (): void => {
+    const board = getBoard();
 
-		board.cardList?.map((item) => {
-			const cardIndex = board.cardList?.indexOf(item);
-			const imageUrl = board.cardList[cardIndex].card?.imageUrl;
-			const cardElement = CardComponent({ imageUrl, indexCard: cardIndex });
+    board.cardList?.map((item) => {
+      const cardIndex = board.cardList?.indexOf(item);
+      const imageUrl = board.cardList[cardIndex].card?.imageUrl;
+      const cardElement = CardComponent({ imageUrl, indexCard: cardIndex });
 
-			cardElement.addEventListener("click", () => {
-				handleFlip(cardIndex, board);
-			});
+      cardElement.addEventListener("click", () => {
+        handleFlip(cardIndex, board);
+      });
 
-			gridContainerElement?.appendChild(cardElement);
-		});
-	};
+      gridContainerElement?.appendChild(cardElement);
+    });
+  };
 
-	const createGrid = () => {
-		scoreboardElement.innerHTML = defaultScoreboard;
-		gridContainerElement
-			?.querySelectorAll("*")
-			.forEach((element) => element.remove());
-		boardContainerElement?.appendChild(gridContainerElement);
-		gridContainerElement?.classList.add("board");
+  const createGrid = () => {
+    scoreboardElement.innerHTML = defaultScoreboard;
+    gridContainerElement
+      ?.querySelectorAll("*")
+      .forEach((element) => element.remove());
+    boardContainerElement?.appendChild(gridContainerElement);
+    gridContainerElement?.classList.add("board");
 
-		createCardList();
-	};
+    createCardList();
+  };
 
-	const onStartGame = () => {
-		startGameButtonElement?.setAttribute("disabled", "true");
-		resetGameButtonElement?.removeAttribute("disabled");
-		startGame();
-		createGrid();
-	};
+  const onStartGame = () => {
+    startGameButtonElement?.setAttribute("disabled", "true");
+    resetGameButtonElement?.removeAttribute("disabled");
+    startGame();
+    createGrid();
+  };
 
-	startGameButtonElement?.addEventListener("click", () => {
-		onStartGame();
-	});
+  startGameButtonElement?.addEventListener("click", () => {
+    onStartGame();
+  });
 
-	resetGameButtonElement?.addEventListener("click", () => {
-		removeGameResultTooltip();
-		// resetGame();
-		onStartGame();
-	});
+  resetGameButtonElement?.addEventListener("click", () => {
+    removeGameResultTooltip();
+    // resetGame();
+    onStartGame();
+  });
 };
