8f573ec07da1ac2a3bd63ebb053ef4796feadfc2
juanpms2 <juanpablo@lemoncode.net>
2023-09-12 13:01:37 +0200

    added layout and new route prefix

--- [13]-banca-online/src/layouts/app/components/navbar.component.tsx
+++ [13]-banca-online/src/layouts/app/components/navbar.component.tsx
@@ -5,28 +5,41 @@ import classes from "./navbar.component.module.css";
 import { useLocation } from "react-router-dom";
 
 export const NavbarComponent: React.FC = () => {
-  const { pathname } = useLocation();
+    const { pathname } = useLocation();
 
-  return (
-    <nav className={classes.navbar}>
-      <ul className={classes.list}>
-        <li
-          className={
-            pathname.startsWith(routesPrefixes.accountList)
-              ? classes.selected
-              : ""
-          }
-        >
-          <Link to={appRoutes.accountList}>Mis Cuentas</Link>
-        </li>
-        <li
-          className={
-            pathname.startsWith(routesPrefixes.transfer) ? classes.selected : ""
-          }
-        >
-          <Link to={appRoutes.transfer}>Transferencias</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className={classes.navbar}>
+            <ul className={classes.list}>
+                <li
+                    className={
+                        pathname.startsWith(routesPrefixes.accountList)
+                            ? classes.selected
+                            : ""
+                    }
+                >
+                    <Link to={appRoutes.accountList}>Mis Cuentas</Link>
+                </li>
+                {pathname.startsWith(routesPrefixes.movements) && (
+                    <li
+                        className={
+                            pathname.startsWith(routesPrefixes.movements)
+                                ? classes.selected
+                                : ""
+                        }
+                    >
+                        <Link to={appRoutes.movements}>Movimientos</Link>
+                    </li>
+                )}
+                <li
+                    className={
+                        pathname.startsWith(routesPrefixes.transfer)
+                            ? classes.selected
+                            : ""
+                    }
+                >
+                    <Link to={appRoutes.transfer}>Transferencias</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 };
