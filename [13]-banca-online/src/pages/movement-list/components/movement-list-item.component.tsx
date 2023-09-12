4b513677559a82ef3a8eac43fb768ab599d9448a
juanpms2 <juanpablo@lemoncode.net>
2023-09-12 15:47:14 +0200

    showed info and completed task

---
+++ [13]-banca-online/src/pages/movement-list/components/movement-list-item.component.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { MovementVm } from "../movement-list.vm";
+import classes from "./movement-list-item.component.module.css";
+
+interface Props {
+    movementItem: MovementVm;
+}
+
+export const MovementListItemComponent: React.FC<Props> = (props) => {
+    const { movementItem } = props;
+    const isNegative = Number(movementItem.amount) < 0;
+
+    return (
+        <div className={classes.row}>
+            <span className={`${classes.dataCell}`}>
+                {movementItem.transaction.toLocaleDateString()}
+            </span>
+            <span className={classes.dataCell}>
+                {movementItem.realTransaction.toLocaleDateString()}
+            </span>
+            <span className={classes.dataCell}>{movementItem.description}</span>
+            <span
+                className={`${classes.dataCell} ${classes.alignRight} ${
+                    isNegative && classes.isNegative
+                }`}
+            >
+                {movementItem.amount} €
+            </span>
+            <span className={`${classes.dataCell} ${classes.alignRight}`}>
+                {movementItem.balance} €
+            </span>
+        </div>
+    );
+};
--- [13]-banca-online/src/pages/movement-list/movement-list.page.tsx
+++ [13]-banca-online/src/pages/movement-list/movement-list.page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AppLayout } from "@/layouts";
 import { useAccountContext } from "@/core/profile/account.context";
 import { getMovementList } from "./api";
+import { MovementListTableComponent } from "./components/movement-list-table.component";
 import { mapMovementListFromApiToVm } from "./movement-list.page.mapper";
 import { MovementVm } from "./movement-list.vm";
 import classes from "./movement-list.page.module.css";
@@ -23,6 +24,10 @@ export const MovementListPage: React.FC = () => {
             <div className={classes.root}>
                 <div className={classes.headerContainer}>
                     <h1>Saldos y Últimos movimientos</h1>
+                    <div className={classes.balanceContainer}>
+                        SALDO DISPONIBLE
+                        <span>{account.balance} €</span>
+                    </div>
                 </div>
                 <div
                     className={`${classes.headerContainer} ${classes.movementInfo}`}
@@ -30,6 +35,7 @@ export const MovementListPage: React.FC = () => {
                     <div>Alias: {account.name}</div>
                     <div>IBAN: {account.iban}</div>
                 </div>
+                <MovementListTableComponent movementList={movementList} />
             </div>
         </AppLayout>
     );
